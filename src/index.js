d35871443ff7e28e11cb1da9f802cea0c9fc2a3e
Mehary Gebremariam <mehary.aynealem3@gmail.com>
2023-07-01 18:27:43 -0500

    display task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  addingEventListeners();
 });
+
+//get the form and add an event listener
+function addingEventListeners() {
+  document
+    .getElementById("create-task-form")
+    .addEventListener("submit", handleFormSubmit);
+}
+
+function handleFormSubmit(e){
+  e.preventDefault()
+//console.log(e)
+  const task = e.target[0].value
+  //const task = e.target["new-task-description"].value
+  //console.log(task)
+  displayTask(task)
+}
+
+function displayTask(task){
+  const taskUl = document.getElementById("tasks")
+  const taskLi = document.createElement("li")
+  taskLi.textContent = task
+  //console.log(taskLi)
+  taskUl.appendChild(taskLi)
+}
---
+++ src/planning.md
@@ -0,0 +1,4 @@
+Stage 1 
+[x] get the form and attach an event listener to the form
+[x] get the task from the inout
+[x] display the data
