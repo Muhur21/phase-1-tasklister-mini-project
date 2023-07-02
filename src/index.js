bb1d0794ce4c997ef5cfbe727e8c349dbd03bd33
Mehary Gebremariam <mehary.aynealem3@gmail.com>
2023-07-02 17:34:25 -0500

    adding the color and priority

--- src/index.js
+++ src/index.js
@@ -15,13 +15,35 @@ function handleFormSubmit(e){
   const task = e.target[0].value
   //const task = e.target["new-task-description"].value
   //console.log(task)
-  displayTask(task)
+  const priorityLevel = parseInt(e.target.priority.value)
+  //console.log(priorityLevel)// These are here just to check if the method is right
+  displayTask(task, priorityLevel)
 }
 
-function displayTask(task){
+function displayTask(task, priorityLevel){
   const taskUl = document.getElementById("tasks")
   const taskLi = document.createElement("li")
-  taskLi.textContent = task
-  //console.log(taskLi)
+const deleteBtn = document.createElement("button")
+
+deleteBtn.textContent = "X"
+deleteBtn.addEventListener("click", deleteTask)
+
+  taskLi.textContent = task + ""
+  taskLi.style.color = getpriorityColor(priorityLevel)
+  taskLi.appendChild(deleteBtn)
   taskUl.appendChild(taskLi)
+}
+function deleteTask(e){
+  e.target.parentNode.remove()
+  //console.log(e)
+}
+
+function getpriorityColor(priorityLevel){
+  if (priorityLevel === 1) {
+    return "red"
+  } else if (priorityLevel === 2){
+    return "blue"
+  } else{
+    return "green"
+  }
 }
--- src/planning.md
+++ src/planning.md
@@ -2,3 +2,10 @@ Stage 1
 [x] get the form and attach an event listener to the form
 [x] get the task from the inout
 [x] display the data
+
+stage 2
+[x] add delete button to each item on the list
+
+stage 3
+[x] select a prioroty level for task before submitting 
+[x] change color of task according to the priority level
